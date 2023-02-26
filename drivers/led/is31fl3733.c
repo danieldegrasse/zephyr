1ab052bbf6cc2ce3466ebb286d40bc4ade847a44
Daniel DeGrasse <daniel@degrasse.com>
2023-02-26 17:45:44 -0600

    wip: new LED algorithm implementation
    Implemented new LED algorithm, which calculates the LED delay
    significatly faster (at least on an M4 core with an FPU)
    
    This algorithm uses log2 functions, which requires newlib nano or
    picolibc. Still trying to figure out how to indicate this in the driver
    Kconfig settings.
    
    Also, this algorithm needs to be tested more throughly. Basic values
    have been tested, but running this on a desktop across a range of ramp
    up, ramp down, and on/off values to verify it returns the same values
    would be useful
    
    Signed-off-by: Daniel DeGrasse <daniel@degrasse.com>

--- drivers/led/is31fl3733.c
+++ drivers/led/is31fl3733.c
@@ -10,6 +10,7 @@
 #include <zephyr/drivers/led.h>
 #include <zephyr/kernel.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define LOG_LEVEL CONFIG_LED_LOG_LEVEL
 #include <zephyr/logging/log.h>
@@ -70,6 +71,9 @@ LOG_MODULE_REGISTER(is31fl3733);
 #define IS31FL3733_ROW_COUNT 12
 #define IS31FL3733_COL_COUNT 16
 #define IS31FL3733_MAX_LED (IS31FL3733_ROW_COUNT * IS31FL3733_COL_COUNT)
+
+/* Constant used in led breathe mode calculations */
+#define LOG2_3_2 0.584962501f
 
 /* Multipliers for led breathe mode calculations
  * These multipliers are derived from the time registers T1-T4.
@@ -216,6 +220,124 @@ static int is31fl3733_abm_solution(uint32_t ramp_up, uint32_t ramp_down,
 }
 
 
+/*
+ * Helper function to find best possible auto breathe mode setting
+ * for requested ramp up, ramp down, on, and off times
+ * returns cumulative error of best solution found.
+ *
+ * Sets t1_b, t2_b, t3_b, t4_b, and pfs_b to best values for
+ * T1,T2,T3,T4, and PFS registers.
+ */
+static int is31fl3733_abm_solution_2(uint32_t ramp_up, uint32_t ramp_down,
+	uint32_t delay_on, uint32_t delay_off, uint8_t *pfs_b, uint8_t *t1_b,
+	uint8_t *t2_b, uint8_t *t3_b, uint8_t *t4_b)
+{
+	float t1, t2, t3, t4;
+	uint32_t pfs_base, err, cand;
+
+	/*
+	 * Since the IS31FL3733 only supports discrete delays,
+	 * we must find the best fit for the PFS and T1-T4 values for
+	 * a given ramp up/down time, and on/off time.
+	 *
+	 * This can be accomplished by minimizing a function calculating
+	 * the difference between the desired ramp up/down time and on/off
+	 * time, and the actual ramp up/down and on/off time for a given PFS
+	 * and T1-T4.
+	 * This function can be modeled with the following equation:
+	 * e = (up - (f*2^t1))^2 + (on - (f*2^(t2 - 1)))^2 +
+	 *	(down - (f*2^t3))^2 + (off - (f*2^(t4 - 1)))^2
+	 * where e is error, and f is the base delay for a given PFS value
+	 * We then calculate the gradient of this function, and find the
+	 * gradient is zero at:
+	 * <T1, T2, T3, T4> = <log2(up/f), log2(on/f) + 1, log2(down/f), log2(off/f)>
+	 *
+	 * Since the PFS base values are not easily differentiable as T1-T4
+	 * values are, use the following algorithm:
+	 * for each PFS base value:
+	 *	calculate zero vector for gradient
+	 *	round each T value to the nearest integer that minimizes error
+	 *	calculate the error function with new Tx values
+	 *	if error is better than current best error:
+	 *		update best error
+	 *		save new Tx and PFS values
+	 *		if error is zero, exit
+	 */
+
+
+	err = UINT32_MAX;
+	for (uint32_t pfs = 0; pfs <= CONF_REG_PFS_MAX; pfs++) {
+		pfs_base = is31fl3733_pfs_time_scale[pfs];
+		/* Calculate the optimal time values for this
+		 * pfs value using the error function's gradient
+		 */
+		t1 = MAX(log2f(((float)ramp_up) / ((float)pfs_base)), 0);
+		t2 = MAX(log2f(((float)delay_on) / ((float)pfs_base)) + 1, 0);
+		t3 = MAX(log2f(((float)ramp_down) / ((float)pfs_base)), 0);
+		t4 = MAX(log2f(((float)delay_off) / ((float)pfs_base)) + 1, 0);
+		/*
+		 * Round each T value in order to minimize the error of
+		 * the function 2^T. We use this rounding because we know
+		 * that in the error function, 2 will be raised to the power
+		 * of T, and traditional integer rounding will not always
+		 * minimize this.
+		 * For a given range [x, x+1], 2^T where log2(x) < T < log2(x+1)
+		 * will be closer to x when: T- floor(T) < log2(3/2)
+		 */
+		if (t1 - floorf(t1) > LOG2_3_2) {
+			t1 = ceilf(t1);
+		} else {
+			t1 = floorf(t1);
+		}
+		if (t2 - floorf(t2) > LOG2_3_2) {
+			t2 = ceilf(t2);
+		} else {
+			t2 = floorf(t2);
+		}
+		if (t3 - floorf(t3) > LOG2_3_2) {
+			t3 = ceilf(t3);
+		} else {
+			t3 = floorf(t3);
+		}
+		if (t4 - floorf(t4) > LOG2_3_2) {
+			t4 = ceilf(t4);
+		} else {
+			t4 = floorf(t4);
+		}
+		if ((t4 == 0) && (pfs == 2)) {
+			/*
+			 * Datasheet says this is an invalid setting.
+			 * Test with T4=1
+			 */
+			t4 = 1;
+		}
+		/* Clamp T1-T4 to their maximum values */
+		t1 = MIN(t1, ABMX_CTRL1_T1_MAX);
+		t2 = MIN(t2, ABMX_CTRL1_T2_MAX);
+		t3 = MIN(t3, ABMX_CTRL2_T3_MAX);
+		t4 = MIN(t4, ABMX_CTRL2_T4_MAX);
+		cand = abs(ramp_up - (pfs_base << ((uint8_t)t1)));
+		cand += abs(delay_on - (pfs_base << (((uint8_t)t2) - 1)));
+		cand += abs(ramp_down - (pfs_base << ((uint8_t)t3)));
+		cand += abs(delay_off - (pfs_base << (((uint8_t)t4) - 1)));
+		if (cand < err) {
+			/* Save new best time values */
+			*t1_b = (uint8_t)t1;
+			*t2_b = (uint8_t)t2;
+			*t3_b = (uint8_t)t3;
+			*t4_b = (uint8_t)t4;
+			*pfs_b = pfs;
+			/* Update best error */
+			err = cand;
+			if (err == 0) {
+				return 0;
+			}
+		}
+	}
+	return err;
+}
+
+
 /* Helper function to turn led on/off */
 static int is31fl3733_led_on_off(const struct device *dev, uint32_t led, bool on)
 {
@@ -280,14 +402,28 @@ static int is31fl3733_led_blink(const struct device *dev, uint32_t led,
 		}
 	}
 
+	uint64_t delta = k_uptime_get();
 	ret = is31fl3733_abm_solution(config->abm_ramp_up,
 				config->abm_ramp_down,
 				delay_on, delay_off,
 				&pfs, &t1, &t2, &t3, &t4);
+	LOG_DBG("ABM solution 1 took %llu ms\n", k_uptime_delta(&delta));
 	LOG_DBG("Found solution for R_UP %d, R_DOWN %d, T_ON %d T_OFF %d",
 		config->abm_ramp_up, config->abm_ramp_down, delay_on, delay_off);
 	LOG_DBG("T1: %d T2: %d T3: %d T4: %d PFS: %d", t1, t2, t3, t4, pfs);
 	LOG_DBG("Error: %d", ret);
+
+	delta = k_uptime_get();
+	ret = is31fl3733_abm_solution_2(config->abm_ramp_up,
+				config->abm_ramp_down,
+				delay_on, delay_off,
+				&pfs, &t1, &t2, &t3, &t4);
+	LOG_DBG("ABM solution 2 took %llu ms\n", k_uptime_delta(&delta));
+	LOG_DBG("Found solution for R_UP %d, R_DOWN %d, T_ON %d T_OFF %d",
+		config->abm_ramp_up, config->abm_ramp_down, delay_on, delay_off);
+	LOG_DBG("T1: %d T2: %d T3: %d T4: %d PFS: %d", t1, t2, t3, t4, pfs);
+	LOG_DBG("Error: %d", ret);
+
 
 	/* Since Zephyr's LED API has no definition for switching modes,
 	 * auto breathe mode 1 is used for the LED blink function. Other
